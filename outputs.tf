ae93750d3c8db20b1832aa6b2bce0541cb6e5dd6
zerotohero200222 <aebharath17@gmail.com>
2025-05-16 13:19:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+output "public_vm_ip" {
+  value = azurerm_public_ip.public_vm_ip.ip_address
+}
+
+output "private_vm_private_ip" {
+  value = azurerm_network_interface.private_vm_nic.private_ip_address
 }
